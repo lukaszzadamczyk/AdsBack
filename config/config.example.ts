7bdd2dd8b898925d45a647ed3993ccf18e5e3a11
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-05-21 13:26:34 +0100

    Fixed connection with DB.

--- config/config.example.ts
+++ config/config.example.ts
@@ -1,7 +1,6 @@
 /**
 export const config = {
   host: 'localhost',
-  port: 8888,
   password: 'password',
   user: 'user',
   database: 'database',
--- utils/db.ts
+++ utils/db.ts
@@ -3,7 +3,6 @@ import { config } from "../config/config";
 
 export const pool = createPool({
     host: config.host,
-    port: config.port,
     password: config.password,
     user: config.user,
     database: config.database,
